09604c8ecd636dd4109169a626e9ed63ee01a471
proGrammer5 <jasoncarrete5@gmail.com>
2014-09-07 16:31:14 -0400

    fixed Resource.java

--- src/com/jasonalexllc/io/Resource.java
+++ src/com/jasonalexllc/io/Resource.java
@@ -1,8 +1,10 @@
 package com.jasonalexllc.io;
 
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 /**
  * Contains helper methods for retrieving resources from jar files
@@ -16,8 +18,20 @@ public final class Resource
 	 */
 	private Resource() {}
 	
-	public static BufferedImage getSpriteSheet(Class c, String imgPath) throws IOException
+	/**
+	 * Returns a BufferedImage that sub-images can be made from.
+	 * @param c The class that is in the same package as the sprite sheet.
+	 * @param imgPath Path of the sprite sheet.
+	 * @return A BufferedImage that represents a sprite sheet.
+	 * @throws IOException
+	 */
+	public static BufferedImage getSpriteSheet(Class<?> c, String imgPath) throws IOException
 	{
 		return ImageIO.read(c.getResource(imgPath));
+	}
+	
+	public static Image getImage(Class<?> c, String imgPath)
+	{
+		return new ImageIcon(c.getResource(imgPath)).getImage();
 	}
 }
