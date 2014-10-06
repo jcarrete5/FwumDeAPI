dd8b1d7021799cc8c894fd8270544335ac230f41
Ryan <EpicPieGuy@users.noreply.github.com>
2014-10-06 19:51:33 -0400

    Added rectangles to Sprite

--- src/com/fwumdegames/games/framework/Sprite.java
+++ src/com/fwumdegames/games/framework/Sprite.java
@@ -1,6 +1,7 @@
 package com.fwumdegames.games.framework;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -16,15 +17,18 @@ public class Sprite extends JComponent
 {
 	private static final long serialVersionUID = -2002162758916094223L;
 	private BufferedImage source;
+	private Rectangle rect;
 	
 	public Sprite(String path) throws IOException
 	{
 		source = ImageIO.read(new File(path));
+		setRectangle();
 	}
 	
 	public Sprite(File source) throws IOException
 	{
 		this.source = ImageIO.read(source);
+		setRectangle();
 	}
 	
 	@Override
@@ -34,9 +38,19 @@ public class Sprite extends JComponent
 		g.drawImage(source,this.getX(), this.getY(), null);
 	}
 	
+	public Rectangle getRectangle()
+	{
+		return rect;
+	}
+	
 	public void dispose()
 	{
 		source.flush();
 		System.gc();
+	}
+	
+	private void setRectangle()
+	{
+		rect = new Rectangle(this.getX(),this.getY(), source.getWidth(), source.getHeight());
 	}
 }
