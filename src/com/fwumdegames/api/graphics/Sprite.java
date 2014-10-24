b67f7e331be48f3107312abfc24a62c91d002c87
Ryan <EpicPieGuy@users.noreply.github.com>
2014-10-23 21:54:25 -0400

    Added Drawable

--- src/com/fwumdegames/api/graphics/Sprite.java
+++ src/com/fwumdegames/api/graphics/Sprite.java
@@ -1,5 +1,7 @@
 package com.fwumdegames.api.graphics;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.Serializable;
@@ -9,12 +11,14 @@ import java.io.Serializable;
  * @author Jason Carrete
  * @since Oct 20, 2014
  */
-public class Sprite extends BufferedImage implements Serializable
+public class Sprite extends BufferedImage implements Serializable, Drawable
 {
 	private static final long serialVersionUID = -7599243419137335696L;
 	
 	public Rectangle hitbox;
 	public Vector2D v;
+	private Color bgcolor;
+	private int stretchWidth, stretchHeight;
 
 	public Sprite(BufferedImage bimg, double x_spd, double y_spd, int x, int y)
 	{
@@ -39,6 +43,49 @@ public class Sprite extends BufferedImage implements Serializable
 	{
 		this(bimg, 0.0, 0.0, 0, 0);
 	}
+	
+	public void setBackgroundColor(Color background)
+	{
+		this.bgcolor = background;
+	}
+	
+	public int getStretchWidth()
+	{
+		return stretchWidth;
+	}
+	
+	public int getStretchHeight()
+	{
+		return stretchHeight;
+	}
+	
+	public void setStretchWidth(int width)
+	{
+		this.stretchWidth = width;
+	}
+	
+	public void setStretchHeight(int height)
+	{
+		this.stretchHeight = height;
+	}
+	
+	/**
+	 * Draws the sprite
+	 * @param g
+	 */
+	public void draw(Graphics g)
+	{
+		if(bgcolor == null)
+			if(stretchWidth == 0 && stretchHeight == 0)
+				g.drawImage(this, (int) hitbox.x, (int) hitbox.y, null);
+			else
+				g.drawImage(this, (int) hitbox.x, (int) hitbox.y, stretchWidth, stretchHeight, null);
+		else
+			if(stretchWidth == 0 && stretchHeight == 0)
+				g.drawImage(this, (int) hitbox.x, (int) hitbox.y, bgcolor, null);
+			else
+				g.drawImage(this, (int) hitbox.x, (int) hitbox.y, stretchWidth, stretchHeight, bgcolor, null);
+	}	
 	
 	/**
 	 * Applies the vector to the position of the sprite.
