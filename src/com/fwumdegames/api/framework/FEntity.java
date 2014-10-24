fbc9bf475250034af14e3644d1891df1337698f2
Ryan <EpicPieGuy@users.noreply.github.com>
2014-10-23 21:11:25 -0400

    Added Entities and environments to framework

---
+++ src/com/fwumdegames/api/framework/FEntity.java
@@ -0,0 +1,10 @@
+package com.fwumdegames.api.framework;
+
+/**
+ * Represents an entity within the game
+ * @author Ryan Goldstein
+ */
+public abstract class FEntity
+{
+	public abstract void update(float delta);
+}
