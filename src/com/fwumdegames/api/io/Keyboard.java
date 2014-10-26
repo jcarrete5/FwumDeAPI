251347b4af6e75ace37d597919d16fef8b747584
Ryan <EpicPieGuy@users.noreply.github.com>
2014-10-26 09:24:16 -0400

    Fixed possible concurrency errors

--- src/com/fwumdegames/api/io/Keyboard.java
+++ src/com/fwumdegames/api/io/Keyboard.java
@@ -11,25 +11,71 @@ import java.util.HashMap;
 public class Keyboard extends KeyAdapter
 {
 	private static HashMap<Integer, Boolean> data;
-	public Keyboard()
+	private static boolean active;
+	static
 	{
 		data = new HashMap<Integer, Boolean>();
+		active = false;
 	}
 	
+	/**
+	 * Creates a new Keyboard object
+	 * @throws KeyboardInstantiatedException Only one keyboard may be instantiated to avoid concurrency errors
+	 */
+	public Keyboard() throws KeyboardInstantiatedException
+	{
+		if(active)
+			throw new KeyboardInstantiatedException();
+		else
+			active = true;
+	}
+	
+	/**
+	 * Checks to see if a key is currently being held down
+	 * Will only work if there has been a Keyboard object added as a KeyListener
+	 * NOTE: FGame will automatically add a Keyboard listener
+	 * @param keyCode The key constant from KeyEvent
+	 * @return The key is being held down on the keyboard
+	 */
 	public static boolean isDown(int keyCode)
 	{
 		return data.containsKey(keyCode) && data.get(keyCode);
 	}
 	
+	/**
+	 * Checks to see if there is an activated keyboard
+	 * @return If there is currently a keyboard object
+	 */
+	public static boolean keyboardActive()
+	{
+		return active;
+	}
+	
 	@Override
+	public void finalize() throws Throwable
+	{
+		super.finalize();
+		active = false;
+	}
+	
+	@Override
+	/**
+	 * Record that the key is now pressed
+	 */
 	public void keyPressed(KeyEvent evt)
 	{
 		data.put(evt.getKeyCode(), true);
 	}
 		
 	@Override
+	/**
+	 * Record that the key is now released
+	 */
 	public void keyReleased(KeyEvent evt)
 	{
 		data.put(evt.getKeyCode(), true);
 	}
+	
+	public class KeyboardInstantiatedException extends Exception
+	{	private static final long serialVersionUID = 1L; }
 }
