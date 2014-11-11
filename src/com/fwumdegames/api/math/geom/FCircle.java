367f52209f1a1cb003034081137372acd285a87b
proGrammer5 <jasoncarrete5@gmail.com>
2014-11-11 15:28:05 -0500

    Enabled the FCircle object to update the x and y positions of the super Ellipse2D.Double

--- src/com/fwumdegames/api/math/geom/FCircle.java
+++ src/com/fwumdegames/api/math/geom/FCircle.java
@@ -2,7 +2,6 @@ package com.fwumdegames.api.math.geom;
 
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
 
 /**
  * Represents a perfect circle.
@@ -14,19 +13,6 @@ public class FCircle extends Ellipse2D.Double
 	private static final long serialVersionUID = 1L;
 	
 	/**
-	 * Instantiates an FCircle object with the specified center point and radius.
-	 * @param center The center coordinate of the circle.
-	 * @param radius The radius of the circle.
-	 */
-	public FCircle(Point2D.Double center, double radius)
-	{
-		super.x = center.x;
-		super.y = center.y;
-		super.width = radius * 2;
-		super.height = radius * 2;
-	}
-	
-	/**
 	 * Instantiates an FCircle object with the specified x and y coordinates and radius.
 	 * @param x The x position of the center point.
 	 * @param y The y position of the center point.
@@ -34,7 +20,20 @@ public class FCircle extends Ellipse2D.Double
 	 */
 	public FCircle(double x, double y, double radius)
 	{
-		this(new Point2D.Double(x, y), radius);
+		super.x = x;
+		super.y = y;
+		super.width = radius * 2;
+		super.height = radius * 2;
+	}
+	
+	/**
+	 * Instantiates an FCircle object with the specified center point and radius.
+	 * @param center The center coordinate of the circle.
+	 * @param radius The radius of the circle.
+	 */
+	public FCircle(Point2D.Double center, double radius)
+	{
+		this(center.x, center.y, radius);
 	}
 	
 	/**
@@ -46,7 +45,7 @@ public class FCircle extends Ellipse2D.Double
 	{
 		Point2D.Double thisCenter = new Point2D.Double(x, y);
 		Point2D.Double otherCenter = new Point2D.Double(other.x, other.y);
-		return (this.width + other.width) < thisCenter.distance(otherCenter); 
+		return (this.width / 2 + other.width / 2) < thisCenter.distance(otherCenter); 
 	}
 	
 	/**
@@ -56,6 +55,8 @@ public class FCircle extends Ellipse2D.Double
 	 */
 	public boolean contains(FCircle other)
 	{
-		return (this.center.distance(other.center) + other.radius) < this.radius;
+		Point2D.Double thisCenter = new Point2D.Double(x, y);
+		Point2D.Double otherCenter = new Point2D.Double(other.x, other.y);
+		return (thisCenter.distance(otherCenter) + other.width / 2) < this.width / 2;
 	}
 }
