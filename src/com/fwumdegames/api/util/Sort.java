ebd260243a681e3b9a8c846ae92221cf27aa14e6
proGrammer5 <jasoncarrete5@gmail.com>
2014-11-12 21:31:29 -0500

    Added quicksort algorithm to Sort.java

--- src/com/fwumdegames/api/util/Sort.java
+++ src/com/fwumdegames/api/util/Sort.java
@@ -7,8 +7,8 @@ package com.fwumdegames.api.util;
 public final class Sort
 {
 	/**
-	 * Uses the selection sort algorithm to sort an array of longs is ascending order.
-	 * @param a array to be sorted
+	 * Uses the selection sort algorithm to sort an array of longs in ascending order.
+	 * @param a Array to be sorted.
 	 */
 	public static void selection(long[] a)
 	{
@@ -26,8 +26,8 @@ public final class Sort
 	}
 	
 	/**
-	 * Uses the selection sort algorithm to sort an array of integers is ascending order.
-	 * @param a array to be sorted
+	 * Uses the selection sort algorithm to sort an array of integers in ascending order.
+	 * @param a Array to be sorted.
 	 */
 	public static void selection(int[] a)
 	{
@@ -45,8 +45,8 @@ public final class Sort
 	}
 	
 	/**
-	 * Uses the selection sort algorithm to sort an array of shorts is ascending order.
-	 * @param a array to be sorted
+	 * Uses the selection sort algorithm to sort an array of shorts in ascending order.
+	 * @param a Array to be sorted.
 	 */
 	public static void selection(short[] a)
 	{
@@ -64,8 +64,8 @@ public final class Sort
 	}
 	
 	/**
-	 * Uses the selection sort algorithm to sort an array of bytes is ascending order.
-	 * @param a array to be sorted
+	 * Uses the selection sort algorithm to sort an array of bytes in ascending order.
+	 * @param a Array to be sorted.
 	 */
 	public static void selection(byte[] a)
 	{
@@ -83,8 +83,8 @@ public final class Sort
 	}
 	
 	/**
-	 * Uses the selection sort algorithm to sort an array of doubles is ascending order.
-	 * @param a array to be sorted
+	 * Uses the selection sort algorithm to sort an array of doubles in ascending order.
+	 * @param a Array to be sorted.
 	 */
 	public static void selection(double[] a)
 	{
@@ -102,8 +102,8 @@ public final class Sort
 	}
 	
 	/**
-	 * Uses the selection sort algorithm to sort an array of floats is ascending order.
-	 * @param a array to be sorted
+	 * Uses the selection sort algorithm to sort an array of floats in ascending order.
+	 * @param a Array to be sorted.
 	 */
 	public static void selection(float[] a)
 	{
@@ -121,8 +121,27 @@ public final class Sort
 	}
 	
 	/**
-	 * Uses the selection sort algorithm to sort an array of Comparable objects is ascending order.
-	 * @param a array to be sorted
+	 * Uses the selection sort algorithm to sort an array of chars in ascending order.
+	 * @param a Array to be sorted.
+	 */
+	public static void selection(char[] a)
+	{
+		for(int pos = 0; pos < a.length; pos++)
+		{
+			int min = pos;
+			for(int target = pos + 1; target < a.length; target++)
+				min = a[target] < a[min] ? target : min;
+			
+			//swap the values
+			char temp = a[pos];
+			a[pos] = a[min];
+			a[min] = temp;
+		}
+	}
+	
+	/**
+	 * Uses the selection sort algorithm to sort an array of Comparable objects in ascending order.
+	 * @param a Array to be sorted.
 	 */
 	public static void selection(Comparable<Object>[] a)
 	{
@@ -139,6 +158,12 @@ public final class Sort
 		}
 	}
 	
+	/**
+	 * Uses the quick sort algorithm to sort an array of longs in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
 	public static void quicksort(long[] a, int left, int right)
 	{
 		if(left >= right)
@@ -158,6 +183,251 @@ public final class Sort
 			if(i <= j) //swap the values
 			{
 				long temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		quicksort(a, left, j);
+		quicksort(a, i, right);
+	}
+	
+	/**
+	 * Uses the quick sort algorithm to sort an array of ints in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
+	public static void quicksort(int[] a, int left, int right)
+	{
+		if(left >= right)
+			return;
+		
+		int i = left;
+		int j = right;
+		int pivot = a[(left + right) / 2];
+		while(i < j)
+		{
+			while(a[i] < pivot)
+				i++;
+			
+			while(a[j] > pivot)
+				j--;
+			
+			if(i <= j) //swap the values
+			{
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		quicksort(a, left, j);
+		quicksort(a, i, right);
+	}
+	
+	/**
+	 * Uses the quick sort algorithm to sort an array of shorts in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
+	public static void quicksort(short[] a, int left, int right)
+	{
+		if(left >= right)
+			return;
+		
+		int i = left;
+		int j = right;
+		short pivot = a[(left + right) / 2];
+		while(i < j)
+		{
+			while(a[i] < pivot)
+				i++;
+			
+			while(a[j] > pivot)
+				j--;
+			
+			if(i <= j) //swap the values
+			{
+				short temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		quicksort(a, left, j);
+		quicksort(a, i, right);
+	}
+	
+	/**
+	 * Uses the quick sort algorithm to sort an array of bytes in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
+	public static void quicksort(byte[] a, int left, int right)
+	{
+		if(left >= right)
+			return;
+		
+		int i = left;
+		int j = right;
+		byte pivot = a[(left + right) / 2];
+		while(i < j)
+		{
+			while(a[i] < pivot)
+				i++;
+			
+			while(a[j] > pivot)
+				j--;
+			
+			if(i <= j) //swap the values
+			{
+				byte temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		quicksort(a, left, j);
+		quicksort(a, i, right);
+	}
+	
+	/**
+	 * Uses the quick sort algorithm to sort an array of doubles in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
+	public static void quicksort(double[] a, int left, int right)
+	{
+		if(left >= right)
+			return;
+		
+		int i = left;
+		int j = right;
+		double pivot = a[(left + right) / 2];
+		while(i < j)
+		{
+			while(a[i] < pivot)
+				i++;
+			
+			while(a[j] > pivot)
+				j--;
+			
+			if(i <= j) //swap the values
+			{
+				double temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		quicksort(a, left, j);
+		quicksort(a, i, right);
+	}
+	
+	/**
+	 * Uses the quick sort algorithm to sort an array of floats in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
+	public static void quicksort(float[] a, int left, int right)
+	{
+		if(left >= right)
+			return;
+		
+		int i = left;
+		int j = right;
+		float pivot = a[(left + right) / 2];
+		while(i < j)
+		{
+			while(a[i] < pivot)
+				i++;
+			
+			while(a[j] > pivot)
+				j--;
+			
+			if(i <= j) //swap the values
+			{
+				float temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		quicksort(a, left, j);
+		quicksort(a, i, right);
+	}
+	
+	/**
+	 * Uses the quick sort algorithm to sort an array of chars in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
+	public static void quicksort(char[] a, int left, int right)
+	{
+		if(left >= right)
+			return;
+		
+		int i = left;
+		int j = right;
+		char pivot = a[(left + right) / 2];
+		while(i < j)
+		{
+			while(a[i] < pivot)
+				i++;
+			
+			while(a[j] > pivot)
+				j--;
+			
+			if(i <= j) //swap the values
+			{
+				char temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		quicksort(a, left, j);
+		quicksort(a, i, right);
+	}
+	
+	/**
+	 * Uses the quick sort algorithm to sort an array of Comparable Objects in ascending order.
+	 * @param a Array to be sorted.
+	 * @param left Leftmost index of the array.
+	 * @param right Rightmost index of the array.
+	 */
+	public static void quicksort(Comparable<Object>[] a, int left, int right)
+	{
+		if(left >= right)
+			return;
+		
+		int i = left;
+		int j = right;
+		Comparable<Object> pivot = a[(left + right) / 2];
+		while(i < j)
+		{
+			while(a[i].compareTo(pivot) < 0)
+				i++;
+			
+			while(a[j].compareTo(pivot) > 0)
+				j--;
+			
+			if(i <= j) //swap the values
+			{
+				Comparable<Object> temp = a[i];
 				a[i] = a[j];
 				a[j] = temp;
 				i++;
