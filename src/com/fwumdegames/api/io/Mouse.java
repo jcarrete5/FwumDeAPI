24d163befb92503a9b666264c3bbbc2dc44f2ed3
EpicPieGuy <ryanisaacg@gmail.com>
2015-01-06 16:06:08 -0500

    Added Mouse class

---
+++ src/com/fwumdegames/api/io/Mouse.java
@@ -0,0 +1,71 @@
+package com.fwumdegames.api.io;
+
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.HashMap;
+
+/**
+ * Holds the data for the mouse
+ * @author Ryan Goldstein
+ */
+public class Mouse extends MouseAdapter implements MouseMotionListener
+{
+	private static final HashMap<Integer, Boolean> buttons;
+	private static final Point position;
+	private static final Mouse instance;
+	
+	static
+	{
+		buttons = new HashMap<Integer, Boolean>();
+		position = new Point();
+		instance = new Mouse();
+	}
+	
+	private Mouse() {}
+	
+	public static Mouse getInstance()
+	{
+		return instance;
+	}
+	
+	/**
+	 * Gets the current mouse position. <br>
+	 * The position is relative to the component the instance was added to.
+	 * @return The (x, y) position of the mouse cursor
+	 */
+	public static Point getPosition()
+	{
+		return position;
+	}
+	
+	/**
+	 * Check to see if a button is pressed
+	 * @param button The key code from MouseEvent
+	 * @return If the button is currently being held
+	 */
+	public static boolean pressed(int button)
+	{
+		return buttons.get(buttons);
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e)
+	{
+		position.x = e.getX();
+		position.y = e.getY();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		buttons.put(e.getButton(), true);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+		buttons.put(e.getButton(), false);
+	}
+}
