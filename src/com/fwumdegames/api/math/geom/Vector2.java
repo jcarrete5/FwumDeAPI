1efca2f41e35a20691a6dd00875ae9488872303d
EpicPieGuy <ryanisaacg@gmail.com>
2015-02-08 19:20:46 -0500

    Added Vector2 class

---
+++ src/com/fwumdegames/api/math/geom/Vector2.java
@@ -0,0 +1,15 @@
+package com.fwumdegames.api.math.geom;
+/**
+ * Backwards-compatible switch of the name of the class
+ * @author Ryan Goldstein
+ */
+public class Vector2 extends Velocity2D
+{
+	private static final long serialVersionUID = 1L;
+
+	public Vector2(double x_spd, double y_spd)
+	{
+		super(x_spd, y_spd);
+	}
+
+}
