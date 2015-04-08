d54c979a8e045c4007b569a53c5ac213f02fc6aa
EpicPieGuy <ryanisaacg@gmail.com>
2015-04-08 13:11:36 -0400

    Switched TileMap to boolean only to allow auto-fill

--- src/com/fwumdesoft/api/math/geom/TileMap.java
+++ src/com/fwumdesoft/api/math/geom/TileMap.java
@@ -1,17 +1,19 @@
 package com.fwumdesoft.api.math.geom;
 
-@SuppressWarnings("unchecked")
-public class TileMap<T>
+import java.util.Arrays;
+
+public class TileMap
 {
-	private final Object[][] map;
+	private final boolean[][] map;
 	public final int TILE, WIDTH, HEIGHT;
 	
 	public TileMap(int tileSize, int widthInTiles, int heightInTiles)
 	{
-		map = new Object[widthInTiles][heightInTiles];
+		map = new boolean[widthInTiles][heightInTiles];
 		TILE = tileSize;
 		WIDTH = TILE * widthInTiles;
 		HEIGHT = TILE * heightInTiles;
+		Arrays.fill(map, false);
 	}
 	
 	public boolean validPoint(int x, int y)
@@ -19,24 +21,24 @@ public class TileMap<T>
 		return x >= 0 && y >= 0 && x / TILE < WIDTH && y / TILE < HEIGHT;
 	}
 	
-	public T setValue(int x, int y, T newVal)
+	public boolean setValue(int x, int y, boolean newVal)
 	{
 		if(!validPoint(x, y))
 			throw new IllegalArgumentException("X and Y coordinates are invalid.");
 		x /= TILE;
 		y /= TILE;
-		T value = (T)map[x][y];
+		boolean value = map[x][y];
 		map[x][y] = newVal;
 		return value;
 	}
 	
-	public T getValue(int x, int y)
+	public boolean getValue(int x, int y)
 	{
 		if(!validPoint(x, y))
 			throw new IllegalArgumentException("X and Y coordinates are invalid.");
 		x /= TILE;
 		y /= TILE;
-		T value = (T)map[x][y];
+		boolean value = map[x][y];
 		return value;
 	}
 }
