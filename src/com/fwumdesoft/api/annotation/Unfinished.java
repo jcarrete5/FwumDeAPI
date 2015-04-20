32e2d80d86616ef1a6a70a71284f359049952aec
proGrammer5 <jasoncarrete5@gmail.com>
2015-04-19 20:32:14 -0400

    added Unfinished annotation

---
+++ src/com/fwumdesoft/api/annotation/Unfinished.java
@@ -0,0 +1,16 @@
+package com.fwumdesoft.api.annotation;
+
+import java.lang.annotation.Documented;
+
+/**
+ * Annotation used to annotate when something is unfinished.<br>
+ * Priority is annotates how much of a priority it is to complete the annotated method or class.
+ * @author Jason Carrete
+ */
+@Documented
+public @interface Unfinished
+{
+	public enum Priority {LOW, MEDIUM, HIGH}
+	
+	Priority priority();
+}
